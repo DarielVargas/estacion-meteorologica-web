5235e18a12d86bdbd03fba7e24ed62c5535f7fea
Dariel Vargas <20181853@ce.pucmm.edu.do>
2025-06-29 00:04:22 -0400

    tabla de estaciones

--- app/src/main/java/org/javadominicano/entidades/DatosHumedad.java
+++ app/src/main/java/org/javadominicano/entidades/DatosHumedad.java
@@ -13,6 +13,9 @@ public class DatosHumedad {
     @Column(name = "sensor_id")
     private String sensorId;
 
+    @Column(name = "estacion_id")
+    private String estacionId;
+
     @Column(name = "humedad")
     private double humedad;
 
@@ -27,6 +30,8 @@ public class DatosHumedad {
 
     public void setId(int id) { this.id = id; }
     public void setSensorId(String sensorId) { this.sensorId = sensorId; }
+    public String getEstacionId() { return estacionId; }
+    public void setEstacionId(String estacionId) { this.estacionId = estacionId; }
     public void setHumedad(double humedad) { this.humedad = humedad; }
     public void setFecha(Timestamp fecha) { this.fecha = fecha; }
 }
