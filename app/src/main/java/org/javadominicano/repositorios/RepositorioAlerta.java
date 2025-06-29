154b78e92c9d14ccef6426ac5f1993cce5b99bc6
Dariel Vargas <20181853@ce.pucmm.edu.do>
2025-06-29 17:58:25 -0400

    extra en alertas

--- app/src/main/java/org/javadominicano/repositorios/RepositorioAlerta.java
+++ app/src/main/java/org/javadominicano/repositorios/RepositorioAlerta.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepositorioAlerta extends JpaRepository<Alerta, Long> {
     Alerta findByNombre(String nombre);
+    long countByActiva(boolean activa);
 }
