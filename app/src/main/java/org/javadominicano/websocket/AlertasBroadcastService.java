1a7f23a3d98a80b1f5a3f0c1713a4aacfe8dda93
Dariel vargas <20181853@ce.pucmm.edu.do>
2025-07-14 13:28:33 -0400

    broadcast alertas via websocket

---
+++ app/src/main/java/org/javadominicano/websocket/AlertasBroadcastService.java
@@ -0,0 +1,25 @@
+package org.javadominicano.websocket;
+
+import org.javadominicano.servicios.AlertasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlertasBroadcastService {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    private AlertasService alertasService;
+
+    @Scheduled(fixedRate = 5000)
+    public void broadcastAlertas() {
+        messagingTemplate.convertAndSend(
+            "/topic/alertas",
+            alertasService.obtenerAlertasActivasTexto()
+        );
+    }
+}
